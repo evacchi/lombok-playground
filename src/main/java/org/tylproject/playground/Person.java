23dcb5937bfdb7a44d323e31c0e8549c979817c7
Edoardo Vacchi <uncommonnonsense@gmail.com>
2014-11-20 09:57:28 +0100

    Initial Commit

---
+++ src/main/java/org/tylproject/playground/Person.java
@@ -0,0 +1,15 @@
+package org.tylproject.playground;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Value;
+import lombok.val;
+
+/**
+ * Created by evacchi on 19/11/14.
+ */
+@Data
+public class Person {
+    private final String firstName;
+    private final String lastName;
+}
---
+++ src/test/java/TestPerson.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.tylproject.playground.Person;
+
+/**
+ * Created by evacchi on 20/11/14.
+ */
+public class TestPerson {
+    @Test
+    public void testPerson() {
+        Person p = new Person("John", "Doe");
+    }
+}
